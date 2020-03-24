cdc3df769a28cec9dc3a70ef38862d8e57a6af4e
Aideen Fay <me@aideen.dev>
2020-03-24 17:36:20 +0000

    feat: add argparse and format data for d3

--- edge_runner.py
+++ edge_runner.py
@@ -2,6 +2,7 @@ import glob
 import json
 from collections import defaultdict
 from hashlib import md5
+from argparse import ArgumentParser
 
 file_list = glob.glob("onionscan_results/*.json")
 
@@ -11,59 +12,141 @@ clearnet_domains = defaultdict(lambda: [])
 ip_addresses = defaultdict(lambda: [])
 
 
+"""
+Generate UID for hidden_service, ip_address and clearnet_service
+"""
+
+
 def get_id(value):
     return value.replace('.', '_').replace(';', '__').replace('&', '_')
 
 
-for json_file in file_list:
+"""
+Generate the JSON data to create Hive chart
+x = 0 is a hidden service node
+x = 1 is an ip address node
+x = 2 is a clearnet service node
 
-    with open(json_file, "rb") as fd:
+"""
 
-        scan_result = json.load(fd)
 
-        hidden_services.add(scan_result['hiddenService'])
-        edges = []
+def create_json(hidden_services, clearnet_domains, ip_addresses):
 
-        if scan_result['linkedSites'] is not None:
-            edges.extend(scan_result['linkedSites'])
-
-        if scan_result['relatedOnionDomains'] is not None:
-            edges.extend(scan_result['relatedOnionDomains'])
-
-        if scan_result['relatedOnionServices'] is not None:
-            edges.extend(scan_result['relatedOnionServices'])
-
-        for edge in edges:
-            if edge.endswith(".onion"):
-                onion_domains[edge].append(scan_result['hiddenService'])
-                hidden_services.add(edge)
-            else:
-                clearnet_domains[edge].append(scan_result['hiddenService'])
-
-        if scan_result['ipAddresses'] is not None:
-            for ip in scan_result['ipAddresses']:
-                ip_addresses[ip].append(scan_result['hiddenService'])
-
-with open("data.dot", "w") as f:
-    f.write("digraph DarkNet {\n")
+    data = {}
+    data["nodes"] = []
+    data["links"] = []
     for hidden_service in hidden_services:
-        f.write(
-            f"{get_id(hidden_service)} [service_type=hidden name={hidden_service} incoming_links={len(onion_domains[hidden_service])}]\n")
-
-    # for onion_domain, hidden_services in onion_domains.items():
-    #     # f.write(
-    #     #     f"{get_id(onion_domain)} [type=hidden_service name={onion_domain}]\n")
-    #     f.writelines(map(
-    #         lambda hidden_service: f"{get_id(hidden_service)} -> {get_id(onion_domain)}\n", hidden_services))
-
-    for clearnet_domain, hidden_services in clearnet_domains.items():
-        f.write(
-            f"{get_id(clearnet_domain)} [service_type=clearnet name={clearnet_domain}]\n")
-        f.writelines(map(
-            lambda hidden_service: f"{get_id(hidden_service)} -> {get_id(clearnet_domain)}\n", hidden_services))
-
+        data["nodes"].append({
+            "id": get_id(hidden_service),
+            "type": "hidden_service",  "name": hidden_service, "x": 0, "y": 0})
     for ip_address, hidden_services in ip_addresses.items():
-        f.write(f"{get_id(ip_address)} [service_type=ip name={ip_address}]\n")
-        f.writelines(map(
-            lambda hidden_service: f"{get_id(hidden_service)} -> {get_id(ip_address)}\n", hidden_services))
-    f.write("}")
+        data["nodes"].append({
+            "id": get_id(ip_address),
+            "type": "hidden_service",  "name": ip_address, "x": 1, "y": 0})
+        for hidden_service in hidden_services:
+            data["links"].append({
+                "source": get_id(ip_address),
+                "target": get_id(hidden_service)
+            })
+    for clearnet_service, hidden_services in clearnet_domains.items():
+        data["nodes"].append({
+            "id": get_id(clearnet_service),
+            "type": "clearnet_service", "name": clearnet_service, "x": 2, "y": 0})
+        for hidden_service in hidden_services:
+            data["links"].append({
+                "source": get_id(clearnet_service),
+                "target": get_id(hidden_service)
+            })
+    with open("hive_data.json", "w") as outfile:
+        json.dump(data, outfile)
+
+
+"""
+Generating .dot file for visualizing data in jhive
+"""
+
+
+def create_dot(hidden_services, clearnet_domains, ip_addresses):
+
+    with open("data.dot", "w") as f:
+        f.write("digraph DarkNet {\n")
+        for hidden_service in hidden_services:
+            f.write(
+                f"{get_id(hidden_service)} [service_type=hidden name={hidden_service} incoming_links={len(onion_domains[hidden_service])}]\n")
+
+        # for onion_domain, hidden_services in onion_domains.items():
+        #     # f.write(
+        #     #     f"{get_id(onion_domain)} [type=hidden_service name={onion_domain}]\n")
+        #     f.writelines(map(
+        #         lambda hidden_service: f"{get_id(hidden_service)} -> {get_id(onion_domain)}\n", hidden_services))
+
+        for clearnet_domain, hidden_services in clearnet_domains.items():
+            f.write(
+                f"{get_id(clearnet_domain)} [service_type=clearnet name={clearnet_domain}]\n")
+            f.writelines(map(
+                lambda hidden_service: f"{get_id(hidden_service)} -> {get_id(clearnet_domain)}\n", hidden_services))
+
+        for ip_address, hidden_services in ip_addresses.items():
+            f.write(
+                f"{get_id(ip_address)} [service_type=ip name={ip_address}]\n")
+            f.writelines(map(
+                lambda hidden_service: f"{get_id(hidden_service)} -> {get_id(ip_address)}\n", hidden_services))
+        f.write("}")
+
+
+"""
+ArgParser to create .dot file or JSON file
+"""
+
+
+def parse_args():
+    parser = ArgumentParser(
+        description="format onionscan data for visualization in D3 or jhive.")
+    parser.add_argument("-json", action='store_true',
+                        help="generate JSON file for D3")
+    parser.add_argument("-dot", action='store_true',
+                        help="generate .dot file for jhive")
+    return parser.parse_args()
+
+
+def main(args):
+
+    for json_file in file_list:
+
+        with open(json_file, "rb") as fd:
+
+            scan_result = json.load(fd)
+
+            hidden_services.add(scan_result['hiddenService'])
+            edges = []
+
+            if scan_result['linkedSites'] is not None:
+                edges.extend(scan_result['linkedSites'])
+
+            if scan_result['relatedOnionDomains'] is not None:
+                edges.extend(scan_result['relatedOnionDomains'])
+
+            if scan_result['relatedOnionServices'] is not None:
+                edges.extend(scan_result['relatedOnionServices'])
+
+            for edge in edges:
+                if edge.endswith(".onion"):
+                    onion_domains[edge].append(scan_result['hiddenService'])
+                    hidden_services.add(edge)
+                else:
+                    clearnet_domains[edge].append(scan_result['hiddenService'])
+
+            if scan_result['ipAddresses'] is not None:
+                for ip in scan_result['ipAddresses']:
+                    ip_addresses[ip].append(scan_result['hiddenService'])
+    if args.json:
+        create_json(hidden_services, clearnet_domains, ip_addresses)
+    elif args.dot:
+        create_dot(hidden_services, clearnet_domains, ip_addresses)
+    else:
+        return
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args=args)
--- script.js
+++ script.js
@@ -1,251 +1,341 @@
+// var width = 960,
+// var height = 850,
+// var innerRadius = 40,
+// var outerRadius = 640,
+//   majorAngle = (2 * Math.PI) / 3,
+//   minorAngle = (1 * Math.PI) / 12;
+
+// var angle = d3.scale
+//   .ordinal()
+//   .domain(["source", "source-target", "target-source", "target"])
+//   .range([0, majorAngle - minorAngle, majorAngle + minorAngle, 2 * majorAngle]);
+
+// var color = d3.scale.category10();
+// var radius = d3.scale.linear().range([innerRadius, outerRadius]);
+// function drawCanvas() {
+//   return d3
+//     .select("body")
+//     .append("svg")
+//     .attr("width", width)
+//     .attr("height", height)
+//     .append("g")
+//     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+// }
+// function drawAxes(svg) {
+//   svg
+//     .selectAll(".axis")
+//     .data(d3.range(3))
+//     .enter()
+//     .append("line")
+//     .attr("class", "axis")
+//     .attr("transform", function(d) {
+//       return "rotate(" + degrees(angle(d)) + ")";
+//     })
+//     .attr("x1", radius.range()[0])
+//     .attr("x2", radius.range()[1]);
+// }
+
 var width = 960,
   height = 850,
   innerRadius = 40,
-  outerRadius = 640,
+  outerRadius = 425,
   majorAngle = (2 * Math.PI) / 3,
   minorAngle = (1 * Math.PI) / 12;
 
 var angle = d3.scale
-  .ordinal()
-  .domain(["source", "source-target", "target-source", "target"])
-  .range([0, majorAngle - minorAngle, majorAngle + minorAngle, 2 * majorAngle]);
+    .ordinal()
+    .domain(d3.range(4))
+    .rangePoints([0, 2 * Math.PI]),
+  radius = d3.scale.linear().range([innerRadius, outerRadius]),
+  color = d3.scale.category10();
 
-var color = d3.scale.category10();
-var radius = d3.scale.linear().range([innerRadius, outerRadius]);
-function drawCanvas() {
-  return d3
-    .select("body")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .appen("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-}
-function drawAxes(svg) {
+var nodes = [
+  { x: 0, y: 0.1 },
+  { x: 0, y: 0.9 },
+  { x: 1, y: 0.2 },
+  { x: 1, y: 0.3 },
+  { x: 2, y: 0.1 },
+  { x: 2, y: 0.8 }
+];
+
+var links = [
+  { source: nodes[0], target: nodes[2] },
+  { source: nodes[1], target: nodes[3] },
+  { source: nodes[2], target: nodes[4] },
+  { source: nodes[2], target: nodes[5] },
+  { source: nodes[3], target: nodes[5] }
+];
+
+// draw Canvas
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
+  .append("g")
+  .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+// draw Axes
+svg
+  .selectAll(".axis")
+  .data(d3.range(3))
+  .enter()
+  .append("line")
+  .attr("class", "axis")
+  .attr("transform", function(d) {
+    return "rotate(" + degrees(angle(d)) + ")";
+  })
+  .attr("x1", radius.range()[0])
+  .attr("x2", radius.range()[1]);
+
+// draw links
+svg
+  .selectAll(".link")
+  .data(links)
+  .enter()
+  .append("path")
+  .attr("class", "link")
+  .attr(
+    "d",
+    d3.hive
+      .link()
+      .angle(function(d) {
+        return angle(d.x);
+      })
+      .radius(function(d) {
+        return radius(d.y);
+      })
+  )
+  .style("stroke", function(d) {
+    return color(d.source.x);
+  })
+  .on("mouseover", linkMouseover)
+  .on("mouseout", mouseout);
+
+// draw nodes
+svg
+  .selectAll(".node")
+  .data(nodes)
+  .enter()
+  .append("circle")
+  .attr("class", "node")
+  .attr("transform", function(d) {
+    return "rotate(" + degrees(angle(d.x)) + ")";
+  })
+  .attr("cx", function(d) {
+    return radius(d.y);
+  })
+  .attr("r", 5)
+  .style("fill", function(d) {
+    return color(d.x);
+  })
+  .on("mouseover", nodeMouseover)
+  .on("mouseout", mouseout);
+
+function drawLinks(svg) {}
+function drawNodes(svg) {
   svg
-    .selectAll(".axis")
-    .data(d3.range(3))
+    .append("g")
+    .attr("class", "nodes")
+    .selectAll(".node")
+    .data(nodes)
     .enter()
-    .append("line")
-    .attr("class", "axis")
-    .attr("transform", function(d) {
-      return "rotate(" + degrees(angle(d)) + ")";
+    .append("g")
+    .attr("class", "node")
+    .style("fill", d => color(d.type))
+    .selectAll("circle")
+    .data(function(d) {
+      return d.connectors;
     })
-    .attr("x1", radius.range()[0])
-    .attr("x2", radius.range()[1]);
+    .enter()
+    .append("circle")
+    .attr("transform", function(d) {
+      return "rotate(" + degrees(angle(d.type)) + ")";
+    })
+    .attr("cx", function(d) {
+      return radius(d.node.index);
+    })
+    .attr("r", 4)
+    .on("mouseover", nodeMouseover)
+    .on("mouseout", mouseout);
 }
-
-function renderGraph() {
-  const svg = drawCanvas();
-  d3.json("master_onionscan_results.json",nodes =>{
-    var nodeTypes = []
-    edges = []
-    nodes.array.forEach(node => {
-        if(node.linkedSites !== ""){
-            edges.push(node.linkedSites)
-        }
-        if(node.relatedOnionDomians !== ""){
-            edges.push(node.relatedOnionDomians)
-        }
-        if(node.relatedOnionServices !== ""){
-            edges.push(node.relatedOnionServices)
-        }
-    
-       if(edges.length){
-           edges.forEach(edge =>{
-               if(edge.endsWith(".onion")){
-                   //add Hidden service node
-                   
-
-               }
-               else{
-                   //add Clearnet node
-                   //add connection between Clearnet node and node.hiddenService
-               }
-               if(node.ipAddresses !== ""){
-                //add IPaddress node
-                //add connection between node IP and associated hideen service node.hiddenService
-            }
-               
-           })
-       }
-    });
-
-})
-
-
-function drawLinks(svg){
-
-}
-function drawNodes(svg){
-    
-}
-
-
-
-
-
-
-
-// Highlight the link and connected nodes on mouseover.
+// highlight link and connected nodes on mouseover
 function linkMouseover(d) {
-    svg.selectAll(".link").classed("active", function(p) { return p === d; });
-    svg.selectAll(".node circle").classed("active", function(p) { return p === d.source || p === d.target; });
-    info.text(d.source.node.name + " → " + d.target.node.name);
-  }
+  svg
+    .selectAll(".link")
+    .classed("turnedOn", function(dl) {
+      return dl === d;
+    })
+    .classed("turnedOff", function(dl) {
+      return !(dl === d);
+    });
+  svg.selectAll(".node").classed("turnedOn", function(dl) {
+    return dl === d.source || dl === d.target;
+  });
+}
 
-  // Highlight the node and connected links on mouseover.
-  function nodeMouseover(d) {
-    svg.selectAll(".link").classed("active", function(p) { return p.source === d || p.target === d; });
-    d3.select(this).classed("active", true);
-    info.text(d.node.name);
-  }
+// highlight node and connected links on mouseover
+function nodeMouseover(d) {
+  svg
+    .selectAll(".link")
+    .classed("turnedOn", function(dl) {
+      return dl.source === d || dl.target === d;
+    })
+    .classed("turnedOff", function(dl) {
+      return !(dl.source === d || dl.target === d);
+    });
+  d3.select(this).classed("turnedOn", true);
+}
 
-  // Clear any highlighted nodes or links.
-  function mouseout() {
-    svg.selectAll(".active").classed("active", false);
-    info.text(defaultInfo);
-  }
-  
+// clear highlighted nodes or links
+function mouseout() {
+  svg.selectAll(".turnedOn").classed("turnedOn", false);
+  svg.selectAll(".turnedOff").classed("turnedOff", false);
+}
+
+function degrees(radians) {
+  return (radians / Math.PI) * 180 - 90;
+}
+
 // A shape generator for Hive links, based on a source and a target.
 // The source and target are defined in polar coordinates (angle and radius).
 // Ratio links can also be drawn by using a startRadius and endRadius.
 // This class is modeled after d3.svg.chord.
-function link() {
-    var source = function(d) {
-        return d.source;
-      },
-      target = function(d) {
-        return d.target;
-      },
-      angle = function(d) {
-        return d.angle;
-      },
-      startRadius = function(d) {
-        return d.radius;
-      },
-      endRadius = startRadius,
-      arcOffset = -Math.PI / 2;
-  
-    function link(d, i) {
-      var s = node(source, this, d, i),
-        t = node(target, this, d, i),
-        x;
-      if (t.a < s.a) (x = t), (t = s), (s = x);
-      if (t.a - s.a > Math.PI) s.a += 2 * Math.PI;
-      var a1 = s.a + (t.a - s.a) / 3,
-        a2 = t.a - (t.a - s.a) / 3;
-      return s.r0 - s.r1 || t.r0 - t.r1
-        ? "M" +
-            Math.cos(s.a) * s.r0 +
-            "," +
-            Math.sin(s.a) * s.r0 +
-            "L" +
-            Math.cos(s.a) * s.r1 +
-            "," +
-            Math.sin(s.a) * s.r1 +
-            "C" +
-            Math.cos(a1) * s.r1 +
-            "," +
-            Math.sin(a1) * s.r1 +
-            " " +
-            Math.cos(a2) * t.r1 +
-            "," +
-            Math.sin(a2) * t.r1 +
-            " " +
-            Math.cos(t.a) * t.r1 +
-            "," +
-            Math.sin(t.a) * t.r1 +
-            "L" +
-            Math.cos(t.a) * t.r0 +
-            "," +
-            Math.sin(t.a) * t.r0 +
-            "C" +
-            Math.cos(a2) * t.r0 +
-            "," +
-            Math.sin(a2) * t.r0 +
-            " " +
-            Math.cos(a1) * s.r0 +
-            "," +
-            Math.sin(a1) * s.r0 +
-            " " +
-            Math.cos(s.a) * s.r0 +
-            "," +
-            Math.sin(s.a) * s.r0
-        : "M" +
-            Math.cos(s.a) * s.r0 +
-            "," +
-            Math.sin(s.a) * s.r0 +
-            "C" +
-            Math.cos(a1) * s.r1 +
-            "," +
-            Math.sin(a1) * s.r1 +
-            " " +
-            Math.cos(a2) * t.r1 +
-            "," +
-            Math.sin(a2) * t.r1 +
-            " " +
-            Math.cos(t.a) * t.r1 +
-            "," +
-            Math.sin(t.a) * t.r1;
-    }
-  
-    function node(method, thiz, d, i) {
-      var node = method.call(thiz, d, i),
-        a =
-          +(typeof angle === "function" ? angle.call(thiz, node, i) : angle) +
-          arcOffset,
-        r0 = +(typeof startRadius === "function"
-          ? startRadius.call(thiz, node, i)
-          : startRadius),
-        r1 =
-          startRadius === endRadius
-            ? r0
-            : +(typeof endRadius === "function"
-                ? endRadius.call(thiz, node, i)
-                : endRadius);
-      return { r0: r0, r1: r1, a: a };
-    }
-  
-    link.source = function(_) {
-      if (!arguments.length) return source;
-      source = _;
-      return link;
-    };
-  
-    link.target = function(_) {
-      if (!arguments.length) return target;
-      target = _;
-      return link;
-    };
-  
-    link.angle = function(_) {
-      if (!arguments.length) return angle;
-      angle = _;
-      return link;
-    };
-  
-    link.radius = function(_) {
-      if (!arguments.length) return startRadius;
-      startRadius = endRadius = _;
-      return link;
-    };
-  
-    link.startRadius = function(_) {
-      if (!arguments.length) return startRadius;
-      startRadius = _;
-      return link;
-    };
-  
-    link.endRadius = function(_) {
-      if (!arguments.length) return endRadius;
-      endRadius = _;
-      return link;
-    };
-  
-    return link;
-  }
-  
-  function degrees(radians) {
-    return (radians / Math.PI) * 180 - 90;
-  }
+// function link() {
+//   var source = function(d) {
+//       return d.source;
+//     },
+//     target = function(d) {
+//       return d.target;
+//     },
+//     angle = function(d) {
+//       return d.angle;
+//     },
+//     startRadius = function(d) {
+//       return d.radius;
+//     },
+//     endRadius = startRadius,
+//     arcOffset = -Math.PI / 2;
+
+//   function link(d, i) {
+//     var s = node(source, this, d, i),
+//       t = node(target, this, d, i),
+//       x;
+//     if (t.a < s.a) (x = t), (t = s), (s = x);
+//     if (t.a - s.a > Math.PI) s.a += 2 * Math.PI;
+//     var a1 = s.a + (t.a - s.a) / 3,
+//       a2 = t.a - (t.a - s.a) / 3;
+//     return s.r0 - s.r1 || t.r0 - t.r1
+//       ? "M" +
+//           Math.cos(s.a) * s.r0 +
+//           "," +
+//           Math.sin(s.a) * s.r0 +
+//           "L" +
+//           Math.cos(s.a) * s.r1 +
+//           "," +
+//           Math.sin(s.a) * s.r1 +
+//           "C" +
+//           Math.cos(a1) * s.r1 +
+//           "," +
+//           Math.sin(a1) * s.r1 +
+//           " " +
+//           Math.cos(a2) * t.r1 +
+//           "," +
+//           Math.sin(a2) * t.r1 +
+//           " " +
+//           Math.cos(t.a) * t.r1 +
+//           "," +
+//           Math.sin(t.a) * t.r1 +
+//           "L" +
+//           Math.cos(t.a) * t.r0 +
+//           "," +
+//           Math.sin(t.a) * t.r0 +
+//           "C" +
+//           Math.cos(a2) * t.r0 +
+//           "," +
+//           Math.sin(a2) * t.r0 +
+//           " " +
+//           Math.cos(a1) * s.r0 +
+//           "," +
+//           Math.sin(a1) * s.r0 +
+//           " " +
+//           Math.cos(s.a) * s.r0 +
+//           "," +
+//           Math.sin(s.a) * s.r0
+//       : "M" +
+//           Math.cos(s.a) * s.r0 +
+//           "," +
+//           Math.sin(s.a) * s.r0 +
+//           "C" +
+//           Math.cos(a1) * s.r1 +
+//           "," +
+//           Math.sin(a1) * s.r1 +
+//           " " +
+//           Math.cos(a2) * t.r1 +
+//           "," +
+//           Math.sin(a2) * t.r1 +
+//           " " +
+//           Math.cos(t.a) * t.r1 +
+//           "," +
+//           Math.sin(t.a) * t.r1;
+//   }
+
+//   function node(method, thiz, d, i) {
+//     var node = method.call(thiz, d, i),
+//       a =
+//         +(typeof angle === "function" ? angle.call(thiz, node, i) : angle) +
+//         arcOffset,
+//       r0 = +(typeof startRadius === "function"
+//         ? startRadius.call(thiz, node, i)
+//         : startRadius),
+//       r1 =
+//         startRadius === endRadius
+//           ? r0
+//           : +(typeof endRadius === "function"
+//               ? endRadius.call(thiz, node, i)
+//               : endRadius);
+//     return { r0: r0, r1: r1, a: a };
+//   }
+
+//   link.source = function(_) {
+//     if (!arguments.length) return source;
+//     source = _;
+//     return link;
+//   };
+
+//   link.target = function(_) {
+//     if (!arguments.length) return target;
+//     target = _;
+//     return link;
+//   };
+
+//   link.angle = function(_) {
+//     if (!arguments.length) return angle;
+//     angle = _;
+//     return link;
+//   };
+
+//   link.radius = function(_) {
+//     if (!arguments.length) return startRadius;
+//     startRadius = endRadius = _;
+//     return link;
+//   };
+
+//   link.startRadius = function(_) {
+//     if (!arguments.length) return startRadius;
+//     startRadius = _;
+//     return link;
+//   };
+
+//   link.endRadius = function(_) {
+//     if (!arguments.length) return endRadius;
+//     endRadius = _;
+//     return link;
+//   };
+
+//   return link;
+// }
+
+// function degrees(radians) {
+//   return (radians / Math.PI) * 180 - 90;
+// }
